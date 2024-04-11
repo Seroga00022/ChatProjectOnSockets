5d5f09b5c7bb210b441b2747e3dc87a6439ebd76
Seroga00022 <sergo00022@gmail.com>
2024-04-11 15:31:03 +0300

    ChatProjectOnSockets

---
+++ src/main/java/org/example/serverchatonsocket/Main.java
@@ -0,0 +1,10 @@
+package org.example.serverchatonsocket;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ChatServer chatServer = new ChatServer();
+        chatServer.serverStart();
+
+    }
+}
---
+++ src/main/java/org/example/serverchatonsocket/MessageRender.java
@@ -0,0 +1,5 @@
+package org.example.serverchatonsocket;
+//Output message.
+public interface MessageRender {
+    public void renderMessage(Message message);
+}
